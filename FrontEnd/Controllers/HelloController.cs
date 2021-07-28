f83444eafa6bf5e1ceb88646f84c4ee8dbce9de5
MORISHIMA Masahito <morishima@pnop.co.jp>
2021-07-28 10:30:38 +0900

    initial commit

---
+++ FrontEnd/Controllers/HelloController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FrontEnd.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HelloController : ControllerBase
+    {
+        private readonly IHttpClientFactory _factory;
+        private readonly ILogger<HelloController> _logger;
+
+        public HelloController(IHttpClientFactory factory, ILogger<HelloController> logger)
+        {
+            _factory = factory;
+            _logger = logger;
+        }
+
+        [HttpGet("hello")]
+        public IActionResult Hello()
+        {
+            return new JsonResult(new HelloResponse {Message = "hello world"});
+        }
+
+        [HttpGet("hello2")]
+        public async Task<IActionResult> Hello2()
+        {
+            var client = _factory.CreateClient();
+            var res = await client.GetAsync(new Uri("http://backend/w"));
+            res.EnsureSuccessStatusCode();
+            return new JsonResult(await res.Content.ReadAsStringAsync());
+        }
+    }
+
+    public class HelloResponse
+    {
+        public String Message { get; set; }
+    }
+}
